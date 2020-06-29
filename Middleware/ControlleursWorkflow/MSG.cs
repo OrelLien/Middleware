3804a5768628c5aed192d8087c30cfed217f2f85
Clément <clement.lesage@viacesi.fr>
2020-06-29 10:29:12 +0200

    Added CAM and MSG classes

---
+++ Middleware/ControlleursWorkflow/MSG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlleursWorkflow
+{
+    struct MSG
+    {
+        public bool statutOp;
+        public string info;
+        public object[] data;
+        public string operationName;
+        public string tokenApp;
+        public string tokenUser;
+        public string appVersion;
+        public string operationVersion;
+    }
+}
